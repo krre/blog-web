9002fb95efd7e174173af0b397866696dbfdecc0
Vladimir Zarypov <krre31@gmail.com>
2026-03-10 09:31:26 +0200

    Add develop net settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
+	server: {
+		port: 3000,
+		host: '127.0.0.1',
+	},
+	preview: {
+		port: 3000,
+		host: '127.0.0.1',
+	},
 	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	test: {
 		expect: { requireAssertions: true },
