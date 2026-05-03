0c5fa79f1c5b633e6c484237da7e8cfccf4ad1b2
Vladimir Zarypov <krre31@gmail.com>
2026-05-03 14:30:56 +0300

    Add login form

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,20 @@
+<form method="POST">
+	<label for="login">Логин: </label>
+	<input type="text" name="login" />
+	<label for="password">Пароль: </label>
+	<input type="password" name="password" />
+	<button>Войти</button>
+</form>
+
+<style>
+	form {
+		display: grid;
+		gap: 0.5em;
+		max-width: 300px;
+		margin: 100px auto;
+	}
+
+	form button {
+		justify-self: end;
+	}
+</style>
