3a1d2cef676ad3c0243417073e69b35cf238700c
Vladimir Zarypov <krre31@gmail.com>
2026-05-07 07:42:03 +0300

    Add `/auth/login` request to API server

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,8 +1,26 @@
 import { API_SERVER_URL } from '$env/static/private';
+import { getRequestEvent } from '$app/server';
 
 export const actions = {
 	default: async ({ cookies, request }) => {
 		const data = await request.formData();
 		console.log({ data, API_SERVER_URL });
+		const body = {
+			username: data.get('username'),
+			password: data.get('password')
+		};
+
+		const { locals, getClientAddress } = getRequestEvent();
+		const response = await fetch(`${API_SERVER_URL}/auth/login`, {
+			method: 'POST',
+			body: JSON.stringify(body),
+			headers: {
+				'content-type': 'application/json',
+				'x-forwarded-for': getClientAddress()
+			}
+		});
+
+		const text = await response.text();
+		console.log({ response, text });
 	}
 };
