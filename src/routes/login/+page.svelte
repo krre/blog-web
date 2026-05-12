1aeb849293c47a4468795aa668e9f5200e8a9089
Vladimir Zarypov <krre31@gmail.com>
2026-05-12 18:33:15 +0300

    Fix redirect to main page on login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import type { PageProps } from './$types';
 	import { i18n } from '$lib/i18n.svelte.js';
 
 	let { form }: PageProps = $props();
 </script>
 
-<form method="POST" use:enhance>
+<form method="POST">
 	<label for="username">{i18n.t('login.username')}: </label>
 	<input type="text" name="username" required />
 	<label for="password">{i18n.t('login.password')}: </label>
