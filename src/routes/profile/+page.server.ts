1556392812f3776f362c21ea7b4a93bae3dbfc65
Vladimir Zarypov <krre31@gmail.com>
2026-05-13 09:33:56 +0300

    Add `/profile` route

---
+++ src/routes/profile/+page.server.ts
@@ -0,0 +1,12 @@
+import * as api from '$lib/api';
+import type { PageServerLoad } from './$types';
+
+interface User {
+	first_name: string;
+	last_name: string;
+}
+
+export const load: PageServerLoad = async ({ locals }) => {
+	let user: User = await api.get(`users/${locals.user?.id}`);
+	return { user };
+};
