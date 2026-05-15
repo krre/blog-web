1ca7205c3a2b7984e655150c4848a5caecc43212
Vladimir Zarypov <krre31@gmail.com>
2026-05-15 09:42:04 +0300

    Add sending profile data to server

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -2,12 +2,26 @@ import * as api from '$lib/api';
 import type { PageServerLoad } from './$types';
 
 interface User {
-	username: string;
-	first_name: string;
-	last_name: string;
+	username?: string;
+	first_name?: string;
+	last_name?: string;
 }
 
 export const load: PageServerLoad = async ({ locals }) => {
 	let user: User = await api.get(`users/${locals.user?.id}`);
 	return { user };
+};
+
+export const actions = {
+	default: async ({ cookies, request, locals }) => {
+		const data = await request.formData();
+
+		const user: User = {
+			first_name: data.get('first_name')?.toString(),
+			last_name: data.get('last_name')?.toString()
+		};
+
+		await api.post(`users/${locals.user?.id}`, user);
+		console.log({ user });
+	}
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -9,11 +9,11 @@
 <Page title={i18n.t('profile.title')}>
 	<form method="POST">
 		<label for="username">{i18n.t('profile.username')}:</label>
-		<input type="text" readonly value={data.user.username} />
-		<label for="first-name">{i18n.t('profile.firstName')}:</label>
-		<input type="text" required value={data.user.first_name} />
-		<label for="last-name">{i18n.t('profile.lastName')}:</label>
-		<input type="text" value={data.user.last_name} />
+		<input type="text" name="username" readonly value={data.user.username} />
+		<label for="first_name">{i18n.t('profile.firstName')}:</label>
+		<input type="text" name="first_name" required value={data.user.first_name} />
+		<label for="last_name">{i18n.t('profile.lastName')}:</label>
+		<input type="text" name="last_name" value={data.user.last_name} />
 		<button>{i18n.t('profile.update')}</button>
 	</form>
 </Page>
