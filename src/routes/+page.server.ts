1997e2df967376bb96cb0bd29f10be7573a11485
Vladimir Zarypov <krre31@gmail.com>
2026-05-23 11:57:54 +0300

    Add post list to main page

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,14 @@
+import * as api from '$lib/api';
+import type { PageServerLoad } from './$types';
+
+interface Post {
+	id: number;
+	title: string;
+	post: string;
+	created_at: Date;
+}
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const posts: Post[] = await api.get('posts');
+	return { posts };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import Page from '$lib/components/Page.svelte';
 	import { i18n } from '$lib/i18n.svelte.js';
+	import { formatDateTime } from '$lib/utils';
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
 </script>
 
 <Page title={i18n.t('main.title')}>
-	<p>{i18n.t('main.underConstruction')}</p>
+	{#each data.posts as post}
+		<p><a href="/posts/{post.id}">{formatDateTime(post.created_at)} {post.title}</a></p>
+	{/each}
 </Page>
