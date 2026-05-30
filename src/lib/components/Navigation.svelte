89af1176ca5886634e2f1a8d4fc9d7dfd55bb2f1
Vladimir Zarypov <krre31@gmail.com>
2026-05-30 13:43:32 +0300

    Apply style to active menu item

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -20,32 +20,53 @@
 </script>
 
 <nav>
-	{#each menuItems as item}
-		{#if item.visible}
-			<a href={item.href}>
-				{item.label}
-			</a>
-		{/if}
-	{/each}
+	<ul>
+		{#each menuItems as item}
+			{#if item.visible}
+				<li>
+					<a
+						href={item.href}
+						class:active={item.href === '/'
+							? page.url.pathname === '/'
+							: page.url.pathname.startsWith(item.href)}
+					>
+						{item.label}
+					</a>
+				</li>
+			{/if}
+		{/each}
+	</ul>
 </nav>
 
 <style>
-	nav {
-		font-size: 1rem;
-	}
-
-	a {
-		padding: 0.3em;
-		text-decoration: none;
-		color: var(--text-color-900);
-	}
-
-	a:hover {
-		color: var(--text-color-600);
-		text-decoration: underline;
-	}
-
-	nav {
+	nav ul {
 		display: flex;
+		font-size: 1rem;
+		list-style: none;
+		gap: 0.875rem;
+		padding: 0;
+	}
+
+	nav a {
+		text-decoration: none;
+		color: var(--text-color-90);
+
+		position: relative;
+		padding-bottom: 5px;
+		transition: color 0.3s ease;
+	}
+
+	nav a:hover {
+		color: var(--text-color-500);
+	}
+
+	nav a.active::after {
+		content: '';
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		height: 2px;
+		background-color: var(--accent-color);
 	}
 </style>
