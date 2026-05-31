c364c43b7557a27f824258a34ede58372fb78929
Vladimir Zarypov <krre31@gmail.com>
2026-05-31 12:41:08 +0300

    Wrap post edit page to `Rectangle.svelte` component

--- src/routes/post/[id]/edit/+page.svelte
+++ src/routes/post/[id]/edit/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Page from '$lib/components/Page.svelte';
+	import Rectangle from '$lib/components/Rectangle.svelte';
 	import PostEditor from '$lib/components/PostEditor.svelte';
 	import { i18n } from '$lib/i18n.svelte.js';
 	import type { PageProps } from './$types';
@@ -15,10 +16,12 @@
 </script>
 
 <Page title={i18n.t('edit.title')}>
-	<PostEditor
-		action="?/edit"
-		title={data.post.title}
-		post={data.post.post}
-		isPublished={data.post.is_published}
-	/>
+	<Rectangle>
+		<PostEditor
+			action="?/edit"
+			title={data.post.title}
+			post={data.post.post}
+			isPublished={data.post.is_published}
+		/>
+	</Rectangle>
 </Page>
