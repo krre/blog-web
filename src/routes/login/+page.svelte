12db849a01867a1b68ed3976920c8c4b3f26b0e0
Vladimir Zarypov <krre31@gmail.com>
2026-05-31 12:53:42 +0300

    Redesign login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,11 +10,21 @@
 
 <Page title={i18n.t('login.title')}>
 	<Rectangle>
-		<Form buttonText={i18n.t('login.loginButton')} error={form?.error}>
-			<label for="username">{i18n.t('login.username')}: </label>
-			<input type="text" id="username" name="username" required value={form?.username ?? ''} />
-			<label for="password">{i18n.t('login.password')}: </label>
-			<input type="password" id="password" name="password" />
-		</Form>
+		<div class="container">
+			<Form buttonText={i18n.t('login.loginButton')} error={form?.error}>
+				<label for="username">{i18n.t('login.username')}: </label>
+				<input type="text" id="username" name="username" required value={form?.username ?? ''} />
+				<label for="password">{i18n.t('login.password')}: </label>
+				<input type="password" id="password" name="password" />
+			</Form>
+		</div>
 	</Rectangle>
 </Page>
+
+<style>
+	.container {
+		margin: 5rem auto;
+		display: flex;
+		justify-content: center;
+	}
+</style>
