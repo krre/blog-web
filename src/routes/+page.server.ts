892a78b85421884a7f5e30696e43bb045ead3e41
Vladimir Zarypov <krre31@gmail.com>
2026-06-01 08:51:27 +0300

    Remove `post` field from `GET /posts` response

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,12 +4,11 @@ import type { PageServerLoad } from './$types';
 interface Post {
 	id: number;
 	title: string;
-	post: string;
 	created_at: Date;
 	is_published: boolean;
 }
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async () => {
 	const posts: Post[] = await api.get('posts');
 	return { posts };
 };
