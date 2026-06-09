3312378c3af4d430f13a3b16f79b1aa64aef7898
Vladimir Zarypov <krre31@gmail.com>
2026-06-09 05:45:55 +0300

    Always use `limit` and `offset` parameters for getting post list

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,16 +5,12 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
 	const page = Number(url.searchParams.get('page')) || 1;
-	let params = null;
+	const limit = consts.Main.PageLimit;
 
-	if (page > 1) {
-		const limit = consts.Main.PageLimit;
-
-		params = {
-			limit,
-			offset: (page - 1) * limit
-		};
-	}
+	const params = {
+		limit,
+		offset: (page - 1) * limit
+	};
 
 	const posts: Post[] = await api.get('posts', params);
 	return { posts };
