515b50e4c7ecc47f303cea4f31a04726d4d683c2
Vladimir Zarypov <krre31@gmail.com>
2026-06-21 10:04:28 +0300

    Add sorting gallery imagers by creating time

--- src/routes/gallery/+page.server.ts
+++ src/routes/gallery/+page.server.ts
@@ -1,8 +1,9 @@
 import { uploadsDir } from '$lib/server-utils';
 import * as consts from '$lib/consts';
 import { error } from '@sveltejs/kit';
-import { readdir } from 'node:fs/promises';
+import { readdir, stat } from 'node:fs/promises';
 import type { PageServerLoad } from './$types';
+import { join } from 'node:path';
 
 interface Images {
 	files: string[];
@@ -12,16 +13,30 @@ interface Images {
 export const load: PageServerLoad = async ({ locals, url }) => {
 	if (!locals.user) return error(401);
 
+	const dirPath = uploadsDir();
+	const files = await readdir(dirPath);
+
+	const filesWithStats = await Promise.all(
+		files.map(async (file) => {
+			const filePath = join(dirPath, file);
+			const fileStat = await stat(filePath);
+
+			return {
+				name: file,
+				mtime: fileStat.mtime.getTime()
+			};
+		})
+	);
+
+	const sortedFiles = filesWithStats.sort((a, b) => b.mtime - a.mtime).map((file) => file.name);
+
 	const page = Number(url.searchParams.get('page')) || 1;
 	const limit = consts.Post.PageLimit;
-
-	const files = await readdir(uploadsDir());
-	files.sort().reverse();
 	const offset = (page - 1) * limit;
 
 	const images: Images = {
-		files: files.slice(offset, offset + limit),
-		total: files.length
+		files: sortedFiles.slice(offset, offset + limit),
+		total: sortedFiles.length
 	};
 
 	return images;
