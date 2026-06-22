18d7e9b1fd60074281e03f0a6f52ab69b161ff2e
Vladimir Zarypov <krre31@gmail.com>
2026-06-22 20:44:27 +0300

    Revert "Fix using `UPLOAD_DIR` runtime environment variable"
    This reverts commit d36cb52afe1670c84d882a6bb11aea26c0a76bc2.

--- src/env.ts
+++ src/env.ts
@@ -1,15 +1,10 @@
 import { defineEnvVars } from '@sveltejs/kit/hooks';
-import { building } from '$app/env';
-import * as v from 'valibot';
 
 export const variables = defineEnvVars({
 	API_SERVER_URL: {
 		static: true
 	},
-	UPLOAD_DIR: {
-		static: false,
-		schema: building ? v.optional(v.string()) : v.string()
-	},
+	UPLOAD_DIR: {},
 	GOOGLE_ANALYTICS_ID: {
 		public: true,
 		static: true
