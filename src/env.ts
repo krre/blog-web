d36cb52afe1670c84d882a6bb11aea26c0a76bc2
Vladimir Zarypov <krre31@gmail.com>
2026-06-22 09:22:01 +0300

    Fix using `UPLOAD_DIR` runtime environment variable

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,15 @@
 import { defineEnvVars } from '@sveltejs/kit/hooks';
+import { building } from '$app/env';
+import * as v from 'valibot';
 
 export const variables = defineEnvVars({
 	API_SERVER_URL: {
 		static: true
 	},
-	UPLOAD_DIR: {},
+	UPLOAD_DIR: {
+		static: false,
+		schema: building ? v.optional(v.string()) : v.string()
+	},
 	GOOGLE_ANALYTICS_ID: {
 		public: true,
 		static: true
