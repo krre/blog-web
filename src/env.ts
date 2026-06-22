25b27f574f40a6458b92f31f839c1a418eabdd2f
Vladimir Zarypov <krre31@gmail.com>
2026-06-22 20:18:08 +0300

    Fix using `UPLOAD_DIR` runtime environment variable
    # Conflicts:
    #	package-lock.json
    #	package.json

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,9 @@ export const variables = defineEnvVars({
 	API_SERVER_URL: {
 		static: true
 	},
-	UPLOAD_DIR: {},
+	UPLOAD_DIR: {
+		static: true
+	},
 	GOOGLE_ANALYTICS_ID: {
 		public: true,
 		static: true
