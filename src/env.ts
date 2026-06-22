e7fc6e0362b37f33a1ca19e75745f839adb8e630
Vladimir Zarypov <krre31@gmail.com>
2026-06-22 20:44:45 +0300

    Revert "Set explicit environment variables to static"
    This reverts commit 47b88bb6dec97cb9fe997ca39a14a4630d08f2be.

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,9 @@
 import { defineEnvVars } from '@sveltejs/kit/hooks';
 
 export const variables = defineEnvVars({
-	API_SERVER_URL: {
-		static: true
-	},
+	API_SERVER_URL: {},
 	UPLOAD_DIR: {},
 	GOOGLE_ANALYTICS_ID: {
-		public: true,
-		static: true
+		public: true
 	}
 });
